ec9567ff76b5781540dc7ddece632795f49fe87a
David Kiss <davidakiss@gmail.com>
2016-02-25 22:27:56 -0500

    setting jar file name to artifactId

--- src/main/java/com/kaviddiss/Application.java
+++ src/main/java/com/kaviddiss/Application.java
@@ -2,9 +2,7 @@ package com.kaviddiss;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.MimeType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
